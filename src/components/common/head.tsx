f0dd3e1713a23e01c7017492471bf807c89c708c
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2022-02-01 09:11:54 +0900

    create footer components

---
+++ src/components/common/head.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import * as React from 'react'
+
+const HomeHead = () => {
+  return (
+    <Head>
+      <title>Kobayashi</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content="Kobayashi" />
+      <meta name="description" content="Kobayashi's Homepage" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
+
+export default HomeHead
---
+++ src/components/link/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { ButtonGroup, ButtonGroupProps, IconButton } from '@chakra-ui/react'
+import { FaGithub, FaTwitter } from 'react-icons/fa'
+
+const Links = (props: ButtonGroupProps) => {
+  return (
+    <ButtonGroup variant="ghost" color="glay.600" {...props}>
+      <IconButton as="a" href='#' aria-label="GitHub" icon={<FaGithub fontSize="20px" />} />
+      <IconButton as="a" href='#' aria-label="Twitter" icon={<FaTwitter fontSize="20px" />} />
+    </ButtonGroup>
+  )
+}
+
+export default Links
