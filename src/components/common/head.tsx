8a1c8389a1f523bfc9f84b98c7e5695953346c2f
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2022-02-07 23:00:37 +0900

    add new meta tags

--- src/components/common/head.tsx
+++ src/components/common/head.tsx
@@ -5,9 +5,16 @@ const HomeHead = () => {
   return (
     <Head>
       <title>Kobayashi</title>
+      <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="author" content="Kobayashi" />
       <meta name="description" content="Kobayashi's Homepage" />
+      <meta property="og:site_name" content="Kobayashi's Homepage" />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="ja_JP" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@0719kobayashi" />
+      <meta name="twitter:creator" content="@0719kobayashi" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   )
