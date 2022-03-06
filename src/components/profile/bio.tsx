9be5d5ff53f3c970ea55cd418bc4529236168284
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2022-03-06 14:06:24 +0900

    integrate bio section to home

--- src/components/profile/bio.tsx
+++ src/components/profile/bio.tsx
@@ -1,67 +1,65 @@
 import * as React from 'react';
-import {Box, Text, List, ListItem} from '@chakra-ui/react';
+import {Text, List, ListItem} from '@chakra-ui/react';
 
-const BioSection = () => {
+const Bio = () => {
   return (
-    <Box as="section" mx="auto" maxW="2xl" py="6" px={{base: '4', md: '8'}}>
-      <List>
-        <ListItem
-          pos="relative"
-          pl="4"
-          pb="6"
-          borderLeft="2px"
-          borderColor="gray.200"
-          _before={{
-            content: `''`,
-            pos: 'absolute',
-            width: '15px',
-            height: '15px',
-            backgroundColor: 'gray.200',
-            borderRadius: '50%',
-            left: -2,
-          }}>
-          <Text fontWeight="semibold">1998</Text>
-          <Text>I was bornn in Nagano.</Text>
-        </ListItem>
-        <ListItem
-          pos="relative"
-          pl="4"
-          pb="6"
-          borderLeft="2px"
-          borderColor="gray.200"
-          _before={{
-            content: `''`,
-            pos: 'absolute',
-            width: '15px',
-            height: '15px',
-            backgroundColor: 'gray.200',
-            borderRadius: '50%',
-            left: -2,
-          }}>
-          <Text fontWeight="semibold">2021</Text>
-          <Text>I graduated from Nihon University of Economics.</Text>
-        </ListItem>
-        <ListItem
-          pos="relative"
-          pl="4"
-          borderLeft="2px"
-          borderColor="gray.200"
-          _before={{
-            content: `''`,
-            pos: 'absolute',
-            width: '15px',
-            height: '15px',
-            backgroundColor: 'gray.200',
-            borderRadius: '50%',
-            left: -2,
-          }}>
-          <Text fontWeight="semibold">2021</Text>
-          <Text>I joined DIP Corporation.</Text>
-          <Text>I work as a frontend engineer.</Text>
-        </ListItem>
-      </List>
-    </Box>
+    <List mt="4" ml="6px">
+      <ListItem
+        pos="relative"
+        pl="4"
+        pb="6"
+        borderLeft="2px"
+        borderColor="gray.200"
+        _before={{
+          content: `''`,
+          pos: 'absolute',
+          width: '15px',
+          height: '15px',
+          backgroundColor: 'gray.200',
+          borderRadius: '50%',
+          left: -2,
+        }}>
+        <Text fontWeight="semibold">1998</Text>
+        <Text>I was born in Nagano.</Text>
+      </ListItem>
+      <ListItem
+        pos="relative"
+        pl="4"
+        pb="6"
+        borderLeft="2px"
+        borderColor="gray.200"
+        _before={{
+          content: `''`,
+          pos: 'absolute',
+          width: '15px',
+          height: '15px',
+          backgroundColor: 'gray.200',
+          borderRadius: '50%',
+          left: -2,
+        }}>
+        <Text fontWeight="semibold">2021</Text>
+        <Text>I graduated from Nihon University of Economics.</Text>
+      </ListItem>
+      <ListItem
+        pos="relative"
+        pl="4"
+        borderLeft="2px"
+        borderColor="gray.200"
+        _before={{
+          content: `''`,
+          pos: 'absolute',
+          width: '15px',
+          height: '15px',
+          backgroundColor: 'gray.200',
+          borderRadius: '50%',
+          left: -2,
+        }}>
+        <Text fontWeight="semibold">2021</Text>
+        <Text>I joined DIP Corporation.</Text>
+        <Text>I work as a frontend engineer.</Text>
+      </ListItem>
+    </List>
   );
 };
 
-export default BioSection;
+export default Bio;
--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {Heading, Box, Text, Image} from '@chakra-ui/react';
-import Links from '../link';
+import Bio from '../profile/bio';
 
 const ProfileSection = () => {
   return (
@@ -11,14 +11,13 @@ const ProfileSection = () => {
             Hi,I&apos;m Hikaru Kobayashi.
           </Heading>
           <Text marginY="4">
-            I&apos;m a frontend engineer working in Tokyo.I work at DIP
-            Corporation as a frontend engineer, and I&apos;m doing personal
-            development.
+            I&apos;m a frontend engineer working in Tokyo.I work at DIP Corporation as a frontend
+            engineer, and I&apos;m doing personal development.
           </Text>
-          <Links />
         </Box>
         {/* <Image rounded="full" maxWidth="200px" maxHeight="200px" src="./images/kobayashi.jpg" alt="Hikaru Kobayashi" margin="auto"/> */}
       </Box>
+      <Bio />
     </Box>
   );
 };
