11bb099de8b4d06f3610333f5b13a85dd780ef3b
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2022-03-11 23:23:26 +0900

    edit footer style

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,15 +4,9 @@ import Links from '../link/index';
 
 const Footer = () => {
   return (
-    <Box
-      as="footer"
-      role="contentinfo"
-      mx="auto"
-      maxW="2xl"
-      py="6"
-      px={{base: '4', md: '8'}}>
+    <Box as="footer" role="contentinfo" mx="auto" maxW="2xl" py="6" px={{base: '4', md: '8'}}>
       <Stack direction="row" spacing="4" align="center" justify="space-between">
-        <Text>Kobayashi</Text>
+        <Text>&copy;&nbsp;{new Date().getFullYear()}&nbsp;Kobayashi</Text>
         <Links />
       </Stack>
     </Box>
