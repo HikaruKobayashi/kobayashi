17d05323368aa7b5c11ab62e37906efe09d44938
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2022-04-02 22:06:54 +0900

    setting profile icon

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {Heading, Box, Text, Image} from '@chakra-ui/react';
-import Bio from '../profile/bio';
 
 const ProfileSection = () => {
   return (
@@ -12,12 +11,21 @@ const ProfileSection = () => {
           </Heading>
           <Text marginY="4">
             I&apos;m a frontend engineer working in Tokyo.I work at DIP Corporation as a frontend
-            engineer, and I&apos;m doing personal development.
+            engineer, and I&apos;m doing personal development.I'm developing native apps outside of
+            work.
           </Text>
         </Box>
-        {/* <Image rounded="full" maxWidth="200px" maxHeight="200px" src="./images/kobayashi.jpg" alt="Hikaru Kobayashi" margin="auto"/> */}
+        <Image
+          rounded="full"
+          width="full"
+          height="150px"
+          maxWidth="150px"
+          maxHeight="150px"
+          src="./images/cafe.jpg"
+          alt="Kobayashi's icon"
+          margin="auto"
+        />
       </Box>
-      <Bio />
     </Box>
   );
 };
