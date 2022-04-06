0e6a563b56a7294caac3b030f1baaa807c16f2c5
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2022-04-06 21:32:06 +0900

    add footer section border

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,7 +5,13 @@ import Links from '../link/index';
 const Footer = () => {
   return (
     <Box as="footer" role="contentinfo" mx="auto" maxW="2xl" py="6" px={{base: '4', md: '8'}}>
-      <Stack direction="row" spacing="4" align="center" justify="space-between">
+      <Stack
+        direction="row"
+        spacing="4"
+        align="center"
+        justify="space-between"
+        borderTop="1px"
+        borderColor="gray.600">
         <Text>&copy;&nbsp;{new Date().getFullYear()}&nbsp;Kobayashi</Text>
         <Links />
       </Stack>
