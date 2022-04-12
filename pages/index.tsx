ebf349ba4308d980c6584224f9b382d5c87530db
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2022-04-12 21:37:26 +0900

    add recent works at home

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import type {NextPage, GetStaticProps} from 'next';
 import * as React from 'react';
 import ProfileSection from '../src/components/profile';
 import BioSection from '../src/components/profile/bio';
+import RecentWork from '../src/components/work/recent';
 
 const Home: NextPage = () => {
   return (
     <>
       <ProfileSection />
       <BioSection />
+      <RecentWork />
     </>
   );
 };
--- src/components/profile/bio.tsx
+++ src/components/profile/bio.tsx
@@ -6,19 +6,19 @@ const Bio = () => {
     <Box as="section" mx="auto" maxW="2xl" px={{base: '4', md: '8'}}>
       <List>
         <ListItem pb="2">
-          <Heading as="h3" size="md" fontWeight="semibold">
+          <Heading as="h2" size="lg" fontWeight="semibold">
             1998
           </Heading>
           <Text>I was born in Nagano.I lived until I graduated from high school.</Text>
         </ListItem>
         <ListItem pb="2">
-          <Heading as="h3" size="md" fontWeight="semibold">
+          <Heading as="h2" size="lg" fontWeight="semibold">
             2017
           </Heading>
           <Text>Entered Nihon University of Economics.Started learning programming.</Text>
         </ListItem>
         <ListItem>
-          <Heading as="h3" size="md" fontWeight="semibold">
+          <Heading as="h2" size="lg" fontWeight="semibold">
             2021
           </Heading>
           <Text>Joined DIP Corporation.I still work as a frontend engineer.</Text>
