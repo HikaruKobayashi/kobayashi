96ecbefb6299059f669835ef9ab002624991d65d
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2022-05-07 12:30:22 +0900

    add smooth scroll function

--- src/components/common/nav-bar.tsx
+++ src/components/common/nav-bar.tsx
@@ -1,4 +1,4 @@
-import {IconButton, Menu, MenuButton, MenuList, MenuItem, Link} from '@chakra-ui/react';
+import {IconButton, Menu, MenuButton, MenuList, MenuItem} from '@chakra-ui/react';
 import {HamburgerIcon} from '@chakra-ui/icons';
 import useLocale from './../../../locales/lang';
 
@@ -8,6 +8,40 @@ interface Props {
 }
 const Navbar = () => {
   const lang = useLocale();
+
+  const smoothScroll = (item: string) => {
+    let target: HTMLElement | null = null;
+    let element = '';
+    switch (true) {
+      case item === 'Top' || item === 'トップ':
+        element = 'top';
+        break;
+      case item === 'Profile' || item === 'プロフィール':
+        element = 'profile';
+        break;
+      case item === 'Biography' || item === '経歴':
+        element = 'bio';
+        break;
+      case item === 'Favorite' || item === '趣味':
+        element = 'favorite';
+        break;
+      case item === 'Work' || item === 'ポートフォリオ':
+        element = 'work';
+        break;
+      case item === 'Contact' || item === 'お問い合わせ':
+        element = 'contact';
+        break;
+      default:
+        break;
+    }
+    target = document.getElementById(element)!;
+    const position = target.getBoundingClientRect().top + window.pageYOffset - 57;
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: 'smooth',
+    });
+  };
 
   return (
     <Menu>
@@ -20,7 +54,10 @@ const Navbar = () => {
       />
       <MenuList>
         {lang.lang.nav.map((item: Props, index: number) => (
-          <MenuItem as={Link} href={'#' + item.link} style={{textDecoration: 'none'}} key={index}>
+          <MenuItem
+            style={{textDecoration: 'none'}}
+            key={index}
+            onClick={() => smoothScroll(item.name)}>
             {item.name}
           </MenuItem>
         ))}
